1e53e2f5790fb008a3f7b301bc090de548f037c3
shreekrushnapowar <80472666+shreekrushnapowar@users.noreply.github.com>
2022-11-24 15:21:07 +0530

    Updates

---
+++ precache-manifest.3a28df72963ee1e6bf784ba453df3a60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f443fb0210bc4f9d17226fa92ae1e570",
+    "url": "/offline/index.html"
+  },
+  {
+    "revision": "0bf24c80bfa2f04f80da",
+    "url": "/offline/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30eae5149c1ced8e69ec",
+    "url": "/offline/static/js/2.89c2f955.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/offline/static/js/2.89c2f955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf24c80bfa2f04f80da",
+    "url": "/offline/static/js/main.8be67c00.chunk.js"
+  },
+  {
+    "revision": "75530354aab029f572be",
+    "url": "/offline/static/js/runtime-main.16542278.js"
+  }
+]);
